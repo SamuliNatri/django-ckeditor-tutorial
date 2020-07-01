9a07d424b176bc8947ccefeeb45ecf4a6eecaa34
Samuli Natri <samuli.natri@gmail.com>
2020-07-01 16:28:41 +0300

    init

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import PostCreateView, PostDetailView
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('<int:pk>/', 
+         PostDetailView.as_view(), 
+         name='detail'),
+    path('create/', 
+         PostCreateView.as_view(), 
+         name='create'),
+]
